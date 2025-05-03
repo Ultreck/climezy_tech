bd1e303d3db7aa1a8f345a4d6c5aa4fa045fc16e
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2025-05-03 21:26:50 +0100

    feat: working on the search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import SearchLocation from './SearchLocation'
 
 const Navbar = () => {
   return (
-    <div className='h-24 w-full bg-slate-900 '>
+    <div className='w-full bg-slate-900 '>
         <nav className="bg-slate-900 w-5xl mx-auto p-4 text-white flex justify-between items-center">
             <div className="text-lg font-bold">Weather App</div>
             <div className="text">
--- src/components/SearchLocation.jsx
+++ src/components/SearchLocation.jsx
@@ -21,10 +21,11 @@ import { useAppContext } from "../context/AppContext";
 import { getWeatherByCity } from "../api/weather";
 
 const CitySelector = ({ disabled, onCityChange }) => {
-    const [openDropdown, setOpenDropdown] = useState(false);
-    const { favorites, setFavorites, setSearchLocation, searchLocation } = useAppContext();
-    const [selectedCity, setSelectedCity] = useState(searchLocation || null);
-  const [searchTerm, setSearchTerm] = useState(searchLocation?.name || '');
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const { favorites, setFavorites, setSearchLocation, searchLocation } =
+    useAppContext();
+  const [selectedCity, setSelectedCity] = useState(searchLocation || null);
+  const [searchTerm, setSearchTerm] = useState("");
   const isFavorite = favorites.includes(selectedCity?.name);
   const toggleFavorite = () => {
     setFavorites((prev) =>
@@ -59,7 +60,7 @@ const CitySelector = ({ disabled, onCityChange }) => {
         setSearchLocation(data);
       } catch (err) {
         console.error(`Failed to fetch weather for ${cityName}`);
-      };
+      }
       const city = { name: cityName };
       setSelectedCity(city);
       onCityChange?.(city);
@@ -73,11 +74,10 @@ const CitySelector = ({ disabled, onCityChange }) => {
     <Popover open={openDropdown} onOpenChange={setOpenDropdown}>
       <PopoverTrigger asChild>
         <Button
-          variant=""
           role="combobox"
           aria-expanded={openDropdown}
           disabled={disabled}
-          className="w-[300px] border justify-between"
+          className="w-[300px] py-6 bg-slate-700 hover:bg-slate-600 justify-between"
         >
           {selectedCity ? selectedCity.name : "Select City..."}
           <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-100" />
@@ -99,20 +99,24 @@ const CitySelector = ({ disabled, onCityChange }) => {
                     key={cityName}
                     value={cityName}
                     onSelect={() => handleCitySelect(cityName)}
-                    className="flex justify-between"
+                    className={`flex ${selectedCity?.name === cityName && 'bg-gray-100'} justify-between`}
                   >
-                    {cityName}
-                    <button onClick={toggleFavorite} className="text-xl">
-                      {isFavorite ? "★" : "☆"}
-                    </button>
-                    <Check
-                      className={cn(
-                        "h-4 w-4",
-                        selectedCity?.name === cityName
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
+                    <div className={`text-base flex justify-between items-center w-full`}>
+                      {cityName}
+                      <div className={`text  flex items-center space-x-2`}>
+                        <button onClick={toggleFavorite} className="text-3xl">
+                          {isFavorite ? "★" : "☆"}
+                        </button>
+                        <Check
+                          className={cn(
+                            "h-5 w-5 text-xl",
+                            selectedCity?.name === cityName
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </div>
+                    </div>
                   </CommandItem>
                 ))}
                 <ScrollBar orientation="vertical" />

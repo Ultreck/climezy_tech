fd08f94a9083ecf2d7092061e445a687d38a6520
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2025-05-03 01:16:18 +0100

    Added the cityList file

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-slate-900 text-white'>
+        This is a test for app.jsx
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/constants/cityLists.js
@@ -0,0 +1,5 @@
+export const TOP_15_CITIES = [
+    "Bangkok", "Beijing", "Buenos Aires", "Cairo", "Delhi",
+    "Dhaka", "Istanbul", "Jakarta", "Karachi", "Lagos",
+    "London", "Los Angeles", "Mexico City", "Mumbai", "Tokyo"
+  ];
